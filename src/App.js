7d6d09374ae9c26859524ca8cda92e19df2fa810
Romildo A. Lima Jr. <odlimor.sevla@gmail.com>
2024-12-02 08:08:06 -0300

    aplicando rotas aos ítens dos produtos.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 //pages
 import Home from './pages/Home';
 import About from './pages/About';
+import Product from './pages/Product';
 
 // components
 import Navbar from './components/Navbar';
@@ -15,15 +16,17 @@ import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
-     <h1>React Router</h1>
-     <BrowserRouter>
-     {/** 2 Links com react router */}
-     <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-     </BrowserRouter>
+      <h1>React Router</h1>
+      <BrowserRouter>
+        {/** 2 Links com react router */}
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          {/** 4 - rota dinamica */}
+          <Route path="/products/:id" element={<Product />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,22 +7,24 @@ const Home = () => {
     // 3 - Carregamento de dados.
     const url = 'http://localhost:3000/products'
 
-    const {data: items, loading, error} = useFetch(url);
-  return (
-    <div>
-        <h1>Produtos</h1>
-        {error && <p>{error}</p>}
-        <ul className="products">
-            {items && items.map(item => (
-                <li key={item.id}>
-                    <h2>{item.name}</h2>
-                    <p>R$.: {item.price}</p>
-                </li>
-            ))}
-        </ul>
-    </div>
+    const { data: items, loading, error } = useFetch(url);
+    return (
+        <div>
+            <h1>Produtos</h1>
+            {error && <p>{error}</p>}
+            <ul className="products">
+                {items && items.map(item => (
+                    <li key={item.id}>
+                        <h2>{item.name}</h2>
+                        <p>R$.: {item.price}</p>
+                        {/** 4 - rota dinamica */}
+                        <Link to={`/products/${item.id}`}>Detalhes</Link>
+                    </li>
+                ))}
+            </ul>
+        </div>
 
-  )
+    )
 }
 
 export default Home
