89145ba5945d0d945baf04b45a4095005fc8fda5
Romildo A. Lima Jr. <odlimor.sevla@gmail.com>
2024-12-02 21:26:33 -0300

    Aplicando o link ativo.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,17 @@
 // 2 - links com react router
 import './Navbar.css';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav>
-      <Link to='/'>Home</Link>
-      <Link to='/about'>About</Link>
+      {/* <Link to='/'>Home</Link>
+      <Link to='/about'>About</Link> */}
+
+      <NavLink to='/' >Home</NavLink>
+      <NavLink to='/about' >Sobre</NavLink>
+      {/* <NavLink to='/' className={({isActive}) => (isActive ? "está-ativo" : "não-ativo")}>Home</NavLink>
+      <NavLink to='/about' className={({isActive}) => (isActive ? "está-ativo" : "não-ativo")}>Sobre</NavLink> */}
     </nav>
   )
 }
