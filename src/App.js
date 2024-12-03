ca17aeba0d1a5647f95a09a696346c73cf7768e9
Romildo A. Lima Jr. <odlimor.sevla@gmail.com>
2024-12-02 22:04:36 -0300

    Aplicando o uso do Redirect.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 
 // 1 - config react router
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 //pages
 import Home from './pages/Home';
@@ -35,6 +35,8 @@ function App() {
           <Route path="/products/:id" element={<Product />} />
           {/** 9 - search */}
           <Route path='/search' element={<Search />} />
+          {/** 10 - redirect */}
+          <Route path="/company" element={<Navigate to="/about" />} />
           {/** 7 - no match route */}
           <Route path='*' element={<NotFound />} />
         </Routes>
